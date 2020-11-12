76f4a8e8d69871359b963e62ed29a39000869cc5
Nabendu <nabendu.biswas@gmail.com>
2020-11-12 16:03:52 +0530

    Part-9 complete

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
-import React, { useReducer } from 'react'
-import './App.css';
-import GrandParent1 from './components/GrandParent1';
-import GrandParent2 from './components/GrandParent2';
-import GrandParent3 from './components/GrandParent3';
-
-export const CountContext = React.createContext()
-
-const initialState = 0
-const reducer = (state, action) => {
-    switch(action) {
-        case 'increment':
-            return state + 1
-        case 'decrement':
-            return state - 1
-        case 'reset':
-            return initialState
-        default:
-            return state
-    }
-}
+import React from 'react'
+import './App.css'
+import DataFetching2 from './components/DataFetching2'
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialState)
-
   return (
-    <CountContext.Provider value={{ countState: count, countDispatch: dispatch}}>
-      <div className="App">
-        Count - {count}
-        <GrandParent1 />
-        <GrandParent2 />
-        <GrandParent3 />
-      </div>
-    </CountContext.Provider>
-  );
+    <div className="App">
+      <DataFetching2 />
+    </div>
+  )
 }
 
-export default App;
+export default App
